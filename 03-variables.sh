24ef01a613a8388b4b86bd78e983820e76a969d0
Chandra Mouly <dklvcm.mtech@gmail.com>
2025-05-19 19:54:48 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1="Modi"       #No spaces before and =
+Person2="Mush"
+
+echo "$Person1: Hi, How are you"
+echo "$Person2: I'm doing good"
+echo "$Person1: Have some coffe"
+echo "$Person2: Thank you"
