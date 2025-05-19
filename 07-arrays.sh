3568399c445dbb42678138053eb484e620e92ec7
Chandra Mouly <dklvcm.mtech@gmail.com>
2025-05-19 20:43:48 +0530

    array

---
+++ 07-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+lord_names=("venkateswara","balaji","srinivasa","govinda")
+
+echo "name1 is $lord_names{0}"
+echo "name2 is $lord_names{1}"
+echo "name3 is $lord_names[2]"
+echo "name4 is $lord_names[3]"
+echo "All names are $lord_names[@]"
+echo "Count of names are $lord_names[#]"
