6103c3104dbe83745c0c790f8f6c6f5e1d996394
Chandra Mouly <dklvcm.mtech@gmail.com>
2025-05-24 12:59:52 +0530

    array

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,7 +5,7 @@ NUMBER=$2
 
 if [ $NUMBER1 -lt $NUMBER2 ]
 then
-	echo "Given number $NUMBER1 is less than $NUMBER2"
+	echo "Given number $(NUMBER1) is less than $(NUMBER2)"
 else
-	echo "Given number $NUMBER1 is greather than $NUMBER2"
+	echo "Given number $(NUMBER1) is greather than $(NUMBER2)"
 fi
