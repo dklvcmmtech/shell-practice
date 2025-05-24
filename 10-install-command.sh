3f5cf0b69848079c2e659f929b8c37c9ebbf3c62
Chandra Mouly <dklvcm.mtech@gmail.com>
2025-05-24 14:33:26 +0530

    install command

---
+++ 10-install-command.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ] then
+	echo "ERROR:: Running script as non root, Please run as root"
+	exit 1
+else
+	echo "Running script as root user..."
+
+dnf list installed mysql
+
+if [ $? -ne 0 ] then
+	echo "MySQL not installed on this System, Installing now ....."
+	dnf install mysql -y
+	if [ $? -eq 0 ] then
+		echo "MySQL installed Successfully"
+	else
+		echo "Unable to install MySQL successfully, Failed"
+		exit 1
+else
+	echo "MySQL already installed"
